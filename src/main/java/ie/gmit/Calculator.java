7d1feef281ae3b7e40dd1d8561715b14ae3a2169
Rokas <G00354068@gmit.ie>
2021-02-11 14:46:38 +0000

    First commit in

---
+++ src/main/java/ie/gmit/Calculator.java
@@ -0,0 +1,16 @@
+package ie.gmit;
+
+public class Calculator {
+
+    int x = 0;
+    int y = 0;
+
+    public Calculator() {
+        x = 1;
+        y = 1;
+    }
+
+    public void add(int firstNum, int secondNum) {
+
+    }
+}
